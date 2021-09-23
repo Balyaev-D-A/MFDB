8c15958f868cd908705395cf7459c3bb3a3765ef
Dmitriy Balyaev <dm.balyaev@gmail.com>
2021-09-23 15:24:22 +0300

    Add comboheader

--- comboheader.cpp
+++ comboheader.cpp
@@ -1,6 +1,42 @@
 #include "comboheader.h"
 
-ComboHeader::ComboHeader(Qt::Orientation orientation, QWidget *parent) : QHeaderView(orientation, parent);
+ComboHeader::ComboHeader(Qt::Orientation orientation, QWidget *parent)
+    : QHeaderView(orientation, parent)
+{
+    margins.left = 0;
+    margins.right = 0;
+    margins.top = 0;
+    margins.bottom = 0;
+}
+
+void ComboHeader::onSectionCountChanged(int oCount, int nCount)
 {
 
+    if (count() > comboList.size())
+        while (comboList.size() < nCount)
+        {
+            comboList.append(new QComboBox(viewport()));
+            comboList.last()->hide();
+            comboList.last()->addItem("-");
+            comboList.last()->addItems(headers);
+        }
+    else
+        while(comboList.size() > nCount)
+            comboList.removeLast();
+    for (int i=0; i<comboList.size(); i++)
+    {
+        comboList[i]->setGeometry(sectionViewportPosition(i) + margins.left,
+                                 margins.top, sectionSize(i) - margins.right,
+                                 height() - margins.bottom);
+        comboList[i]->show();
+    }
 }
+
+void ComboHeader::onSectionResized(int lIndex, int oSize, int nSize)
+{
+
+    comboList[lIndex].setGeometry(sectionViewportPosition(lIndex) + margins.left,
+                                  margins.top, nSize - margins.right,
+                                  height() - margins.bottom);
+}
+
--- comboheader.h
+++ comboheader.h
@@ -2,12 +2,33 @@
 #define COMBOHEADER_H
 
 #include <QHeaderView>
+#include <QComboBox>
 
 class ComboHeader : public QHeaderView
 {
     Q_OBJECT
 public:
     ComboHeader(Qt::Orientation orientation, QWidget *parent);
+    void setHeaders(QStringList headers);
+
+private:
+    struct Margins {
+        int left;
+        int right;
+        int top;
+        int bottom;
+    } margins;
+    QList<QComboBox*> comboList;
+    QStringList headers;
+
+protected:
+//    void showEvent(QShowEvent *e);
+
+private slots:
+    void onSectionCountChanged(int oCount, int nCount);
+    void onSectionResized(int lIndex, int oSize, int nSize);
+
+
 };
 
 #endif // COMBOHEADER_H
